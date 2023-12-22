9ecd2524189a496b920605052715196132610b88
Saugat-Shrestha <Shresthaasaugat31@gmail.com>
2023-12-22 21:59:35 +0545

    animatedcontainer

--- lib/widgets/Popular_bar.dart
+++ lib/widgets/Popular_bar.dart
@@ -1,14 +1,40 @@
 import 'package:FoodDeliveryApp/pages/Item_Page.dart';
 import 'package:flutter/material.dart';
 
-class PopularBar extends StatelessWidget {
+class PopularBar extends StatefulWidget {
   const PopularBar({super.key});
 
+  @override
+  State<PopularBar> createState() => _PopularBarState();
+}
+
+class _PopularBarState extends State<PopularBar> {
   void _openProduct(BuildContext context) {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => const ItemPage()),
     );
+  }
+
+  double _currentWidth = 180.0;
+  double _currentHeight = 265.0;
+  double _originalWidth = 180.0;
+  double _originalHeight = 265.0;
+  double _increasedWidth = 230.0;
+  double _increasedHeight = 315.0;
+
+  void changeSideLength() {
+    setState(() {
+      // _widthLength = 230;
+      // _heightLength = 315;
+
+      _currentWidth =
+          (_currentWidth == _originalWidth) ? _increasedWidth : _originalWidth;
+
+      _currentHeight = (_currentHeight == _originalHeight)
+          ? _increasedHeight
+          : _originalHeight;
+    });
   }
 
   @override
@@ -20,79 +46,85 @@ class PopularBar extends StatelessWidget {
         child: Row(children: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Container(
-                width: 180,
-                height: 265,
-                padding: EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 3),
-                        spreadRadius: 2,
-                        blurRadius: 10,
-                        color: Colors.grey.withOpacity(0.5),
-                      )
-                    ]),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          // Navigator.pushNamed(context, "itemPage");
-                          _openProduct(context);
-                        },
-                        child: Hero(
-                          tag: Image,
-                          child: Container(
-                            alignment: Alignment.center,
-                            child: Image.asset(
-                              "assets/images/burger.png",
-                              height: 130,
+            child: GestureDetector(
+              onTap: changeSideLength,
+              child: AnimatedContainer(
+                  duration: Duration(
+                    milliseconds: 500,
+                  ),
+                  width: _currentWidth,
+                  height: _currentHeight,
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 3),
+                          spreadRadius: 2,
+                          blurRadius: 10,
+                          color: Colors.grey.withOpacity(0.5),
+                        )
+                      ]),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            // Navigator.pushNamed(context, "itemPage");
+                            _openProduct(context);
+                          },
+                          child: Hero(
+                            tag: Image,
+                            child: Container(
+                              alignment: Alignment.center,
+                              child: Image.asset(
+                                "assets/images/burger.png",
+                                height: 130,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Text(
-                        "Hot Burger",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Text(
-                        "Taste Our Hot Burgers ",
-                        style: TextStyle(
-                          fontSize: 15,
+                        Text(
+                          "Hot Burger",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
                         ),
-                      ),
-                      SizedBox(
-                        height: 12,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "\$10",
-                            style: TextStyle(
-                                color: Colors.red,
-                                fontSize: 17,
-                                fontWeight: FontWeight.bold),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          "Taste Our Hot Burgers ",
+                          style: TextStyle(
+                            fontSize: 15,
                           ),
-                          Icon(
-                            Icons.favorite_border,
-                            color: Colors.red,
-                            size: 26,
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                )),
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "\$10",
+                              style: TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Icon(
+                              Icons.favorite_border,
+                              color: Colors.red,
+                              size: 26,
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  )),
+            ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
